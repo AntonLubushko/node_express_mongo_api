204bc6df4bb8f9c544396815fc902ed0ba3abf14
Nton <antonlubushko@gmail.com>
2018-12-14 17:34:35 +0200

    made little refactors

--- src/server.js
+++ src/server.js
@@ -20,14 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-
-// routes
-// app.use((req, res, next) => {
-// 	const err = new Error("Not Found");
-// 	err.status = 404;
-// 	next(err);
-// });
-
+app.use("/api/auth", routes.auth);
 app.use((err, req, res, next) => {
 	res.status(err.status || 500);
 	res.send({
@@ -65,8 +58,6 @@ app.get("/artists/:id", async (req, res) => {
 		res.sendStatus(404);
 	}
 });
-
-app.use("/api/auth", routes.auth);
 
 app.post("/artists", async (req, res) => {
 	try {
